9f62a9c41f040050f42f5c3e8c962ea725b063cc
emperorsixpacks <andrewdvd10@gmail.com>
2025-04-23 15:27:39 +0100

    feat: get token decentralisation_score

--- src/services.py
+++ src/services.py
@@ -1,20 +1,53 @@
 import asyncio
+from urllib.parse import urlencode, urlunparse
 
 import httpx
 from playwright.async_api import async_playwright
 
-BUBBLE_MAPS_API_URL = "https://api-legacy.bubblemaps.io/map-data"
-ElementsToRemove = [".mdc-top-app-bar", "div.buttons-row:nth-child(6)", ".buttons-row"]
+BUBBLE_MAPS_API_URL = "https://api-legacy.bubblemaps.io"
+ELEMENTS_TO_REMOVE = [
+    ".mdc-top-app-bar",
+    "div.buttons-row:nth-child(6)",
+    ".buttons-row",
+]
 
 
-async def get_token_bubble_map(contract_addres: str, chain: str) -> dict[str, any]:
+async def get_token_bubble_map(*, contract_addres: str, chain: str) -> dict[str, any]:
+    path = "map-data"
+    qparams = {
+        "chain": chain,
+        "token": contract_addres,
+    }
+    query_string = urlencode(qparams)
+
+    url = f"{BUBBLE_MAPS_API_URL}/{path}?{query_string}"
+
+    response = await send_requet(url)
+    if response.get("message") == "Data not available for this token":
+        return None
+    return response
+
+
+async def get_decentralization_score(
+    *, contract_addres: str, chain: str
+) -> dict[str, any]:
+    path = "map-metadata"
+    qparams = {
+        "chain": chain,
+        "token": contract_addres,
+    }
+    query_string = urlencode(qparams)
+
+    url = f"{BUBBLE_MAPS_API_URL}/{path}?{query_string}"
+    response = await send_requet(url)
+    if response.get("message") == "Data not available for this token":
+        return None
+    return response
+
+
+async def send_requet(url: str):
     async with httpx.AsyncClient() as client:
-        url = BUBBLE_MAPS_API_URL
-        params = {
-            "chain": chain,
-            "token": contract_addres,
-        }
-        response = await client.get(url, params=params)
+        response = await client.get(url)
         data = response.json()
         return data
 
@@ -29,7 +62,7 @@ async def get_bubble_map_screenshot(chain: str, contract_addres: str):
         await asyncio.sleep(3)
         await page.evaluate(
             "document.querySelectorAll('{0}').forEach(el => el.remove());".format(
-                ",".join(ElementsToRemove)
+                ",".join(ELEMENTS_TO_REMOVE)
             )
         )
         await page.locator(".graph-view").screenshot(path="screenshot.png")
