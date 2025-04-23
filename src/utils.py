8f0ecd039059ce41c149cdb77d2c4f7ef4767385
emperorsixpacks <andrewdvd10@gmail.com>
2025-04-23 17:27:54 +0100

    feat: send_request and ensure_screenshots_directory functions

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,21 @@
 import os
 
+import httpx
+
 
 def return_base_dir():
     return os.path.dirname(os.path.dirname(__file__))
+
+
+async def send_request(url: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        return response
+
+
+def ensure_screenshots_directory():
+    base_dir = return_base_dir()
+    upload_dir = os.path.join(base_dir, "screenshots")
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+    return upload_dir
