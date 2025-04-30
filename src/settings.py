83322b91ad3e321e5104df705d0fce1b87065ffc
emperorsixpacks <andrewdvd10@gmail.com>
2025-04-30 22:25:28 +0100

    refactor

--- src/settings.py
+++ src/settings.py
@@ -1,27 +1,29 @@
 import os
 
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from utils import return_base_dir
 
-# Load the .env file manually
-# env_path = os.path.join(return_base_dir(), ".env")
-# load_dotenv(env_path)
+env_dir = os.path.join(return_base_dir(), ".env")
 
 
-class TelegramSettings:
-    def __init__(self):
-        self.telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+class AppSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=env_dir, env_file_encoding="utf-8", extra="ignore"
+    )
 
 
-class CoinGeckoAPISettings:
-    def __init__(self):
-        self.coin_gecko_api_key = os.getenv("COIN_GECKO_API_KEY")
+class TelegramSettings(AppSettings):
+    telegram_bot_token: str
 
 
-class IBMSettings:
-    def __init__(self):
-        self.ibm_service_endpoint = os.getenv("IBM_SERVICE_ENDPOINT")
-        self.ibm_bucket_name = os.getenv("IBM_BUCKET_NAME")
-        self.ibm_bucket_instance_id = os.getenv("IBM_BUCKET_INSTANCE_ID")
-        self.ibm_api_key = os.getenv("IBM_API_KEY")
+class CoinGeckoAPISettings(AppSettings):
+    coin_gecko_api_key: str
+
+
+class IBMSettings(AppSettings):
+    ibm_service_endpoint: str
+    ibm_bucket_name: str
+    ibm_bucket_instance_id: str
+    ibm_api_key: str
+
